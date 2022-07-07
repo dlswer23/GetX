e73b1d0ea98398e35b572cee9763601252f04208
dlswer23 <lbj4298@naver.com>
2022-07-07 11:45:03 +0900

    [UPDATE] cart controller

---
+++ shopping_app/lib/controller/cart_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:shopping_app/model/product.dart';
+
+class CartController extends GetxController{
+
+  var cartItems = <Product>[].obs;
+  double get totalPrice => cartItems.fold(0, (e, item) => e + item.price);
+  int get count => cartItems.length;
+
+  void addToItem(Product product){
+    cartItems.add(product);
+  }
+
+}
